3f34311cc07809cf6f253ee64c2301223865e2ee
wjj <jiajun7355@qq.com>
2017-08-15 17:53:23 +0800

    no message

---
+++ src/main/java/com/wjj/jdbc/test/Test.java
@@ -0,0 +1,27 @@
+package com.wjj.jdbc.test;
+
+import io.searchbox.core.SearchResult;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+public class Test {
+    public static void testJDBC() throws Exception{
+        Class.forName("com.wjj.jdbc.elasticsearch.ElasticSearchDriver");
+        Connection conn = DriverManager.getConnection("jdbc:elasticsearch://192.168.70.128:9300");
+        PreparedStatement stmt = conn.prepareStatement("select * from bank");
+        ResultSet rs = stmt.executeQuery();
+        while (rs.next()){
+            System.out.println("firstname:"+rs.getString("firstname")+",balance:"+rs.getInt("balance"));
+        }
+    }
+    public static void main(String[] args) throws Exception{
+        testJDBC();
+    }
+}
